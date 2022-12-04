fd75f0e09e32e7f6d7e2cf4b5890a7af87881d4f
JaehyoJJAng <yshrim12@naver.com>
2022-12-04 20:03:14 +0900

    '숫자 비교하기'

---
+++ algorithm/python_practice/01_숫자비교하기/script.py
@@ -0,0 +1,8 @@
+"""
+숫자 비교하기
+Author : GitHub <yshrim12@naver.com>
+"""
+
+def solutuon(num1,num2):
+    pass
+
---
+++ commit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# 커밋 자동화 스크립트
+
+status=$(git status --porcelain | awk '{print $2}' | head -n 1)
+GIT=$(which git)
+
+if [[ ! -e ${status} ]]
+then
+  echo "${status} File not Found"
+  exit 1
+fi
+
+# Get Commit Message
+commitMsg=$(find ${PWD} -iname "*.py" -exec bash -c "cat {}" \; | grep "[가-힣ㄱ-ㅎ]")
+
+# Git Commit
+${GIT} add -A
+${GIT} status
+${GIT} commit -m "'${commitMsg}'"
+${GIT} push -u origin main
+
+echo "${status} Commit Done"
