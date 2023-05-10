553699866f371957ab2a64c5f2988c8667c19470
JaehyoJJang <yshrim12@naver.com>
2023-05-10 14:17:06 +0900

    알파벳 출력하기 - string 모듈

---
+++ algorithm/programmers/python-like-python/05_알파벳 출력.py
@@ -0,0 +1,14 @@
+import string
+
+""" 
+import string 
+
+string.ascii_lowercase # 소문자 abcdefghijklmnopqrstuvwxyz
+string.ascii_uppercase # 대문자 ABCDEFGHIJKLMNOPQRSTUVWXYZ
+string.ascii_letters # 대소문자 모두 abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+string.digits # 숫자 0123456789
+"""
+
+num = int(input())
+answer = string.ascii_lowercase if num == 0 else string.ascii_uppercase
+print(answer)
