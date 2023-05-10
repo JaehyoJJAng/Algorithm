651ab057a4c2d0745320e020fe4ca805ee699cec
JaehyoJJang <yshrim12@naver.com>
2023-05-11 08:16:55 +0900

    코드 수정

--- algorithm/programmers/python-like-python/10_map 함수 응용하기.py
+++ algorithm/programmers/python-like-python/10_map 함수 응용하기.py
@@ -1,6 +1,6 @@
 from typing import List
 def solution(mylist: List[List[int]]) -> List[int]:
-    return list(map(int,mylist))
+    return list(map(len,mylist))
 
 mylist : List[List[int]] = [ [1], [2] ]
-solution(mylist=mylist)
+print(solution(mylist=mylist))
