8810a02c226030fcf08bfcf62bd0f1630c70d627
JaehyoJJang <yshrim12@naver.com>
2023-05-11 20:24:45 +0900

    문자열 겹쳐쓰기

---
+++ algorithm/programmers/lv0/11_문자열 겹쳐쓰기.py
@@ -0,0 +1,10 @@
+def solution(my_string: str, overwrite_string: str, s: int) -> str:
+    my_string = list(my_string)
+    for idx in range(len(overwrite_string)):
+        my_string[s + idx] = overwrite_string[idx]
+    return ''.join(my_string)
+
+my_string : str = 'He11oWor1d'
+overwrite_string : str = 'lloWorl'
+s : int = 2
+solution(my_string=my_string,overwrite_string=overwrite_string,s=s)
