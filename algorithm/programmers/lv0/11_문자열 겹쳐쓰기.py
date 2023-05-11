0c7659f1afe514f132ae6ca3d922b36dd26aa0e0
JaehyoJJang <yshrim12@naver.com>
2023-05-11 20:25:44 +0900

    문자열 겹쳐쓰기

--- algorithm/programmers/lv0/11_문자열 겹쳐쓰기.py
+++ algorithm/programmers/lv0/11_문자열 겹쳐쓰기.py
@@ -1,8 +1,10 @@
 def solution(my_string: str, overwrite_string: str, s: int) -> str:
-    my_string = list(my_string)
-    for idx in range(len(overwrite_string)):
-        my_string[s + idx] = overwrite_string[idx]
-    return ''.join(my_string)
+    # my_string = list(my_string)
+    # for idx in range(len(overwrite_string)):
+    #     my_string[s + idx] = overwrite_string[idx]
+    # return ''.join(my_string)
+    
+    return my_string[:s] + overwrite_string + my_string[s + len(overwrite_string)]
 
 my_string : str = 'He11oWor1d'
 overwrite_string : str = 'lloWorl'
