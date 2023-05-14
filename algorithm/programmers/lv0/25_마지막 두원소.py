0081a25104b15a2ce053073a64129b2f070d5d02
JaehyoJJang <yshrim12@naver.com>
2023-05-14 15:46:49 +0900

    마지막 두 원소

--- algorithm/programmers/lv0/25_마지막 두원소.py
+++ algorithm/programmers/lv0/25_마지막 두원소.py
@@ -1,24 +1,14 @@
 from typing import List
 
-def solution(num_list: List[int]) -> List[int]:
-    # length = len(num_list)
-    # answer : List[int] = [0 for _ in range(2)]
-    
-    # for i in range(length):
-    #     if i == length - 1:
-    #         answer[-1] = num_list[i]
-    #     elif i == length - 2:
-    #         answer[0] = num_list[i]
-    
-    # if answer[-1] > answer[0]:
-    #     num = answer[-1] - answer[0]
-    #     num_list.append(num)
+def solution(num_list: List[int]) -> List[int]:        
+    # === 내 풀이 ===
+    # if num_list[-1] > num_list[-2]:
+    #     num_list.append(num_list[-1] - num_list[-2])
     # else:
-    #     num = answer[-1] * 2
-    #     num_list.append(num)
-    
+    #     num_list.append(num_list[-1] * 2)
     # return num_list
-    
+
+    # === 다른 사람 풀이 ===\
     return num_list.append(num_list[-1] - num_list[-2] if num_list[-1] > num_list[-2] else num_list[-1] * 2)
     
 num_list : List[int] = [5,2,1,7,5]
